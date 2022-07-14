f3fb8758a20045c9f4f10ed23fed4393245dec2d
khananas-dev <ak8433786864@gmail.com>
2022-07-14 11:09:31 +0530

    socket url changed

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:8000";
+// const URL = "http://localhost:8000";
+const URL = "https://basic-chat-app-server.herokuapp.com";
 const socket = io(URL, { autoConnect: false });
 
 export default socket;
